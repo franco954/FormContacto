259a1c9d489d27137fbc098f8eee318e8d87c213
Franco Nuñez <fnunez@novared.net>
2021-04-24 18:18:04 -0300

    formulario tp1 finalizado

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
 
 
+function mostrarPrecio(precio){
+    switch (precio) {
+        case 1:
+            document.querySelector("body > main > form > div > p:nth-child(3)").style.opacity = "1"
+            document.querySelector("body > main > form > div > p:nth-child(6)").style.opacity = "0"
+            document.querySelector("body > main > form > div > p:nth-child(9)").style.opacity = "0"
+            break;
+        case 2:
+            document.querySelector("body > main > form > div > p:nth-child(3)").style.opacity = "0"
+            document.querySelector("body > main > form > div > p:nth-child(6)").style.opacity = "1"
+            document.querySelector("body > main > form > div > p:nth-child(9)").style.opacity = "0"
+            break;
+        case 3:
+            document.querySelector("body > main > form > div > p:nth-child(3)").style.opacity = "0"
+            document.querySelector("body > main > form > div > p:nth-child(6)").style.opacity = "0"
+            document.querySelector("body > main > form > div > p:nth-child(9)").style.opacity = "1"
+            break;
+        default:
+            console.log("Parametro invalido")
+            break;
+    }
+}
 
- 
+
+
+
+
+
+
+
